10fff3164ce7894f35043c2e7e4530b9eed42402
Bush Philip <fp.bush@gmail.com>
2022-06-25 12:51:55 +0300

    Implemented shape-module

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -1,5 +1,40 @@
 import {Module} from '../core/module'
+import {random} from '../utils';
 
 export class ShapeModule extends Module {
+  constructor(type, text) {
+    super(type, text)
+  }
 
+  removeShape() {
+    const previousShape = document.querySelector('.random-shape');
+    if (previousShape) {
+      previousShape.remove()
+    }
+  }
+
+  trigger() {
+    this.removeShape();
+
+    const screen = document.querySelector('body');
+    const screenSize = screen.getBoundingClientRect();
+    const shapeSizeWidth = random(screenSize.width/20, screenSize.width/3);
+    const shapeSizeHeight = random(screenSize.width/20, screenSize.height/3);
+    const shapeLeftMax = screenSize.width - shapeSizeWidth;
+    const shapeTopMax = screenSize.height - shapeSizeHeight;
+    const shapeColor = `rgb(${random(0, 255)},${random(0, 255)}, ${random(0, 255)})`;
+    const borderRadius = `${random(0, 20)}%`;
+    const shape = document.createElement('div');
+
+    shape.className = 'random-shape';
+    shape.style.position = 'absolute';
+    shape.style.backgroundColor = shapeColor;
+    shape.style.borderRadius = borderRadius;
+    shape.style.width = shapeSizeWidth + 'px';
+    shape.style.height = shapeSizeHeight + 'px';
+    shape.style.left = random(0, shapeLeftMax) + 'px';
+    shape.style.top = random(0, shapeTopMax) + 'px';
+
+    screen.append(shape);
+  }
 }
