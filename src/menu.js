215380c2a187d88f726f8435e386339b4cfa7f61
Bush Philip <fp.bush@gmail.com>
2022-06-25 16:13:06 +0300

    добавил вывод пункта контекстног меню

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,24 @@
-import {Menu} from './core/menu'
+import {Menu} from './core/menu';
+import {ShapeModule} from './modules/shape.module';
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector)
+  }
 
+  open() {
+    const shape = new ShapeModule('shape', 'Создать фигуру');
+
+    const menu = document.querySelector('.menu');
+    const menuItemShape = shape.toHTML();
+    menu.innerHTML += menuItemShape;
+    menu.classList.add('open');
+
+    menu.addEventListener('click', event => {
+      switch(event.target.dataset.type) {
+        case 'shape':
+          shape.trigger();
+      }
+    });
+  }
 }
