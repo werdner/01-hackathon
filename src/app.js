25f8af351cc383d75d6abc8634061d6811641839
Greg <greg@MacBook-Pro-di-User.local>
2022-06-25 16:19:01 +0200

    add contextMenu

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import './styles.css'
+import { ContextMenu } from './menu'
 
-import { CallMessageModule } from './modules/call-message.module'
-
+const contextMenu = new ContextMenu('#menu');
+contextMenu.open()
+contextMenu.add()
+contextMenu.trigger()
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,34 @@
 import {Menu} from './core/menu'
+import { CallMessageModule } from './modules/call-message.module';
+
+const callMessageModule = new CallMessageModule('clickModule', 'Аналитика кликов');
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+  }
 
+  open() {
+    const contextMenu = document.querySelector('#menu');
+    document.body.addEventListener('contextmenu', event => {
+      event.preventDefault();
+
+      contextMenu.style.top = `${event.clientY}px`;
+      contextMenu.style.left = `${event.clientX}px`;
+      contextMenu.classList.add('open');
+
+      document.body.addEventListener('click', event => {
+        if (event.button !== 2) {
+          contextMenu.classList.remove('open');
+        }
+      })
+    });
+  }
+  add() {
+    const contextMenu = document.querySelector('#menu');
+    contextMenu.insertAdjacentHTML('beforeend', callMessageModule.toHTML());
+  }
+  trigger() {
+    // callMessageModule.trigger();
+  }
 }
