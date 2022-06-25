87a5c17ddfa42cd56d6e1b4a1af31f26abdd42c4
Greg <greg@MacBook-Pro-di-User.local>
2022-06-25 20:21:43 +0200

    edit call-message.module.js

--- src/modules/call-message.module.js
+++ src/modules/call-message.module.js
@@ -6,11 +6,6 @@ export class CallMessageModule extends Module {
     super(type, text);
   }
 
-  renderMenuItem() {
-    const menuListHTML = document.querySelector('#menu');
-    menuListHTML.insertAdjacentHTML('afterbegin', callMessage.toHTML());
-  }
-
   createMessageBlock(text, addClass) {
     return `
     <div id="message-area">
@@ -20,33 +15,33 @@ export class CallMessageModule extends Module {
 
   removeMessageBlock() {
     const messageBlockHTML = document.querySelector('#message-area');
-    messageBlockHTML.remove();
+    messageBlockHTML.remove()
   }
 
   trigger() {
-    const clickModalItem = document.querySelector('[data-type = "clickModule"]');
-    clickModalItem.addEventListener('click', (event) => {
-    const messageItemClasses = ['success', 'error', 'info'];
-    const messageText = [
-      'success: cupiditate doloremque dolorum, ea facere itaque labore',
-      'error: incidunt non omnis quam quidem quod reiciendis',
-      'info: Deserunt excepturi minima quo repellendus tempora'
-    ];
-    let randomNumber = random(1,3);
-    const messageBlockHTML = this.createMessageBlock(
-      `${messageText[randomNumber -1]}`, `${messageItemClasses[randomNumber -1]}`
-    );
-    document.body.insertAdjacentHTML('afterbegin', messageBlockHTML);
-    setTimeout(() => {
-      this.removeMessageBlock();
-    }, 3000);
+    const menuMessageItem = document.querySelector('[data-type="message-text"]');
+    menuMessageItem.addEventListener('click', (event) => {
+      const messageItemClasses = ['success', 'error', 'info'];
+      const messageText = [
+        'success: cupiditate doloremque dolorum, ea facere itaque labore',
+        'error: incidunt non omnis quam quidem quod reiciendis',
+        'info: Deserunt excepturi minima quo repellendus tempora'
+      ];
+
+      const isMessageBlockHTML = document.querySelector('#message-area');
+      if (isMessageBlockHTML) {
+        this.removeMessageBlock()
+      }
+
+      let randomNumber = random(1,3);
+      const messageBlockHTML = this.createMessageBlock(
+        `${messageText[randomNumber -1]}`, `${messageItemClasses[randomNumber -1]}`
+      );
+      document.body.insertAdjacentHTML('afterbegin', messageBlockHTML);
+      setTimeout(() => {
+        this.removeMessageBlock();
+      }, 3000);
     })
   }
 
-  eventClickMenuItem() {
-    const menuMessageItem = document.querySelector('[data-type="message-text"]');
-    menuMessageItem.addEventListener('click', (event) => {
-      this.trigger();
-    })
-  }
 }
