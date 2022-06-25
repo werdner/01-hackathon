3ac1281f514926abe3774fc0b0efedbf2e726938
Greg <greg@MacBook-Pro-di-User.local>
2022-06-25 09:46:00 +0200

    implementation call-message module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,4 @@
 import './styles.css'
 
+import { CallMessageModule } from './modules/call-message.module'
+
--- src/modules/call-message.module.js
+++ src/modules/call-message.module.js
@@ -1,7 +1,50 @@
 import {Module} from '../core/module'
+import {random} from "@/utils";
 
 export class CallMessageModule extends Module {
+  constructor(type, text) {
+    super(type, text);
+  }
+
+  renderMenuItem() {
+    const menuListHTML = document.querySelector('#menu');
+    menuListHTML.insertAdjacentHTML('afterbegin', callMessage.toHTML());
+  }
+
+  createMessageBlock(text, addClass) {
+    return `
+    <div id="message-area">
+        <div class="message-item ${addClass}">${text}</div>
+    </div>`
+  }
+
+  removeMessageBlock() {
+    const messageBlockHTML = document.querySelector('#message-area');
+    messageBlockHTML.remove();
+  }
+
+  trigger() {
+    const messageItemClasses = ['success', 'error', 'info'];
+    const messageText = ['success', 'error', 'info'];
+    let randomNumber = random(1,3);
+    const messageBlockHTML = this.createMessageBlock(
+      `${messageText[randomNumber -1]}`, `${messageItemClasses[randomNumber -1]}`
+    );
+    document.body.insertAdjacentHTML('afterbegin', messageBlockHTML);
+    setTimeout(() => {
+      this.removeMessageBlock();
+    }, 3000);
+  }
+
+  eventClickMenuItem() {
+    const menuMessageItem = document.querySelector('[data-type="message-text"]');
+    menuMessageItem.addEventListener('click', (event) => {
+      this.trigger();
+    })
+  }
 
 }
 
-console.log('hello')
+const callMessage = new CallMessageModule('message-text', 'Вызвать сообщение');
+callMessage.renderMenuItem();
+callMessage.eventClickMenuItem();
