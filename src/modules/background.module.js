2b0ee870a117a1784b673f81ead0c28de030a03f
werdner <werdner.roman@gmail.com>
2022-06-26 23:34:27 +0300

    added styles for background.modules

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,9 +1,12 @@
 import { Module } from '../core/module';
 import { getRandomColor } from '../utils';
+import { random } from '../utils';
 
 export class BackgroundModule extends Module {
   getColor() {
-    document.body.style = `background: ${getRandomColor()}`;
+    const body = document.body;
+    body.style.backgroundColor = `${getRandomColor()}`;
+    body.style.transition = `background-color ${random(0.2, 2)}s`;
   }
 
   trigger(event) {
